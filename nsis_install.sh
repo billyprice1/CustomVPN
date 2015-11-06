3c295f8d44327cd1957d0b6c27c4cefb11b90dcc
makerling <d7e5fbb0@opayq.com>
2015-11-06 05:15:29 -0500

    edit nsis installer to reflect changes in install to pritunl

--- nsis_install.sh
+++ nsis_install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 echo "beginning nsis and pritunl install script"
-sudo add-apt-repository -y ppa:pritunl &&
 echo
-echo "updating repos, and installing pritunl, might take a minute..."
-sudo apt-get -q=2 update &&
-echo
+#old code commented out before pritunl install changed
+#sudo add-apt-repository -y ppa:pritunl &&
+#echo
+#echo "updating repos, and installing pritunl, might take a minute..."
+#sudo apt-get -q=2 update &&
+#echo
 #sudo apt-get upgrade -y &&
-sudo apt-get -q=2 install pritunl nsis unzip -y &&
+#sudo apt-get -q=2 install pritunl nsis unzip -y &&
+echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.0.list
+echo "deb http://repo.pritunl.com/stable/apt trusty main" > /etc/apt/sources.list.d/pritunl.list
+echo
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 7F0CEB10
+apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv CF8E292A
+apt-get --assume-yes -q=2 update &&
+apt-get --assume-yes -q=2 upgrade &&
+apt-get --assume-yes -q=2 install pritunl mongodb-org nsis unzip &&
+service pritunl start
 clear
 echo "successfully installed software, now installing/configuring Shelllink plugin for nsis"
 sleep 4
